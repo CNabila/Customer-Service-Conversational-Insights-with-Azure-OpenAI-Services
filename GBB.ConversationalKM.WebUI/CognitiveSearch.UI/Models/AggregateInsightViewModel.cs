6aac2e069c99d3f4014f2be7d6b35841191e7ac9
CNabila <161469893+CNabila@users.noreply.github.com>
2024-06-04 17:21:36 +0400

    Update AggregateInsightViewModel.cs

--- GBB.ConversationalKM.WebUI/CognitiveSearch.UI/Models/AggregateInsightViewModel.cs
+++ GBB.ConversationalKM.WebUI/CognitiveSearch.UI/Models/AggregateInsightViewModel.cs
@@ -8,12 +8,13 @@ namespace CognitiveSearch.UI.Models
     {
         public string KeyInsight1 { get; set; }
         public string KeyInsight2 { get; set; }
+        public string KeyInsight2 { get; set; }
 
         public List<string> TopInsights { get; set; }
 
         public AggregateInsightViewModel()
         {
-            TopInsights = new List<string>();
+            
         }
     }
 }
